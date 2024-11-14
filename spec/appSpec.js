b5ceb1a0da9aba56195558ab7173b678f2f4695d
NGOMA UMBA Tamalie <128788295+tamalie-ngomaUmba-vinci@users.noreply.github.com>
2024-11-14 15:02:40 +0100

    all lamp functions + test

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -4,23 +4,91 @@ const main = new Main();
 
 describe("Berlin clock", () => {
 
-    it("should light up yellow lamps every single minute", function() {
-        expect('oo').toBe('O');
-        expect(main.lampSingleMinute(3)).toBe('OOO');
-        expect(main.lampSingleMinute(17)).toBe('OO');
+    it("should light up yellow lamps every minute in the last line", function() {
+        expect(main.lampSingleMinute(1)).toBe('Y');
+        expect(main.lampSingleMinute(3)).toBe('YYY');
+        expect(main.lampSingleMinute(5)).toBe('');
+        expect(main.lampSingleMinute(17)).toBe('YY');
     });
 
-    it("should turn on a red light every 3 lights and the others are yellow", function(){
-        expect(main.lampBlockFiveMinute(5)).toBe('1Y');
-        expect(main.lampBlockFiveMinute(10)).toBe('2Y');
-        expect(main.lampBlockFiveMinute(15)).toBe('2YR');
+    it("should turn on a red light every 3 lights, the others being yellow in the 5-minute block line", function(){
+        expect(main.lampBlockFiveMinute(5)).toBe('Y');
+        expect(main.lampBlockFiveMinute(10)).toBe('YY');
+        expect(main.lampBlockFiveMinute(11)).toBe('YY');
         expect(main.lampBlockFiveMinute(50)).toBe('YYRYYRYYRY');
     });
     
-    it("should turn on red lights every single hour", function(){
-        expect(main.lampSingleHour(1)).toBe('1R');
-        expect(main.lampSingleHour(3)).toBe('1R');
-        expect(main.lampSingleHour(17)).toBe('3RRR');
+    it("should turn on the red lights every hour in the simple hour line", function(){
+        expect(main.lampSingleHour(1)).toBe('R');
+        expect(main.lampSingleHour(3)).toBe('RRR');
+        expect(main.lampSingleHour(17)).toBe('RR');
     });
+
+    it("should light red lamps every 5 hours in the first line", function(){
+        expect(main.lampBlockFiveHour(5)).toBe('R');
+        expect(main.lampBlockFiveHour(10)).toBe('RR');
+        expect(main.lampBlockFiveHour(11)).toBe('RR');
+        expect(main.lampBlockFiveHour(20)).toBe('RRRR');
+    });
+
+    it("should light up red during even seconds and go out during odd seconds", function(){
+        expect(main.lampSecond(2)).toBe('R');
+        expect(main.lampSecond(1)).toBe('');
+        expect(main.lampSecond(3)).toBe('');
+        expect(main.lampSecond(16)).toBe('R');
+        expect(main.lampSecond(57)).toBe('');
+    });
+
+    it("should do all the above - full clock", function() {
+        // object
+        expect(main.lampFullTime.not.toBe(null));
+
+        /* lampSecond
+        expect(main.lampFullTime(0,0,0))[0].toBe('R');
+        expect(main.lampFullTime(0,0,2))[0].toBe('R');
+        expect(main.lampFullTime(0,0,4))[0].toBe('R');
+        expect(main.lampFullTime(0,0,18))[0].toBe('R');
+        expect(main.lampFullTime(0,0,56))[0].toBe('R');
+
+        expect(main.lampFullTime(0,0,1))[0].toBe('');
+        expect(main.lampFullTime(0,0,3))[0].toBe('');
+        expect(main.lampFullTime(0,0,15))[0].toBe('');
+        expect(main.lampFullTime(0,0,39))[0].toBe('');
+        expect(main.lampFullTime(0,0,59))[0].toBe('');
+        
+
+        // lampBlockFiveHour
+        expect(main.lampFullTime(0,0,0))[1].toBe('');
+        expect(main.lampFullTime(1,0,0))[1].toBe('');       
+        expect(main.lampFullTime(5,0,0))[1].toBe('R');
+        expect(main.lampFullTime(8,0,0))[1].toBe('R');        
+        expect(main.lampFullTime(10,0,0))[1].toBe('RR');
+        expect(main.lampFullTime(11,0,0))[1].toBe('RR');
+        expect(main.lampFullTime(21,0,0))[1].toBe('RRRR');
+        expect(main.lampFullTime(23,0,0))[1].toBe('RRRR');
+
+        // lampSingleHour
+        expect(main.lampFullTime(0,0,0))[2].toBe('');
+        expect(main.lampFullTime(1,0,0))[2].toBe('R');       
+        expect(main.lampFullTime(5,0,0))[2].toBe('R');
+        expect(main.lampFullTime(8,0,0))[2].toBe('RRR');        
+        expect(main.lampFullTime(10,0,0))[2].toBe('');
+        expect(main.lampFullTime(11,0,0))[2].toBe('R');
+        expect(main.lampFullTime(21,0,0))[2].toBe('R');
+        expect(main.lampFullTime(23,0,0))[2].toBe('RRR');
+
+        // lampBlockFiveMinute
+        expect(main.lampFullTime(0,5,0))[3].toBe('Y');
+        expect(main.lampFullTime(0,10,0))[3].toBe('YY');
+        expect(main.lampFullTime(0,11,0))[3].toBe('YY');
+        expect(main.lampFullTime(0,50,0))[3].toBe('YYRYYRYYRY');
+
+        // lampSingleMinute
+        expect(main.lampFullTime(0,1,0))[4].toBe('Y');
+        expect(main.lampFullTime(0,3,0))[4].toBe('YYY');
+        expect(main.lampFullTime(0,5,0))[4].toBe('');
+        expect(main.lampFullTime(0,17,0))[4].toBe('YY');*/
+
+    })
 
 });
--- src/app.js
+++ src/app.js
@@ -1,19 +1,46 @@
 export class Main{
 
-        lampSingleMinute(minute) {
-            let lamp = minute % 5
-            return 'Y'.repeat(lamp);
-        }  
-        
-        lampBlockFiveMinute(minute){
-            let result = '';
-
-            for(let i = 5; i <= minute; i+=5){
-                if(i % 15 == 0)
-                    result += 'R';
-                else result += 'Y';
-            }
-
-            return result;
+    lampSingleMinute(minute) {
+        return 'Y'.repeat(minute % 5);
+    }  
+    
+    lampBlockFiveMinute(minute){
+        let result = '';
+        for(let i = 5; i <= minute; i+=5){
+            if(i % 15 == 0)
+                result += 'R';
+            else result += 'Y';
         }
+        return result;
+    }
+
+    lampSingleHour(hour){
+        return 'R'.repeat(hour % 5);
+    }
+
+    lampBlockFiveHour(hour){
+        return 'R'.repeat(Math.floor(hour/5));
+    }
+
+    lampSecond(second){
+        return second % 2 == 0 ? 'R' : '';  
+    }
+
+    lampFullTime(hour, minute, second) {
+
+        //  0: lampSecond            
+        //  1:lampBlockFiveHour    
+        //  2:lampSingleHour
+        //  3:lampBlockFiveMinute    
+        //  4:lampSingleMinute
+
+        let berlinHourValues = [this.lampSecond(second), 
+                                this.lampBlockFiveHour(hour), 
+                                this.lampSingleHour(hour), 
+                                this.lampBlockFiveMinute(minute), 
+                                this.lampSingleMinute(minute)   ];               
+        
+        return berlinHourValues;               
+
+    }
 }
