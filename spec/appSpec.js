25ac6d12d20a7e2a7cbeab7fb7d63f3eb1f8320f
NGOMA UMBA Tamalie <128788295+tamalie-ngomaUmba-vinci@users.noreply.github.com>
2024-11-14 12:19:40 +0100

    test avec toutes les valeurs erronées =>aucun specs found

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -5,22 +5,22 @@ const main = new Main();
 describe("Berlin clock", () => {
 
     it("should light up yellow lamps every single minute", function() {
-        expect(main.lampSingleMinute(1)).toBe('Y');
-        expect(main.lampSingleMinute(3)).toBe('YYY');
-        expect(main.lampSingleMinute(17)).toBe('YY');
+        expect('oo').toBe('O');
+        expect(main.lampSingleMinute(3)).toBe('OOO');
+        expect(main.lampSingleMinute(17)).toBe('OO');
     });
 
     it("should turn on a red light every 3 lights and the others are yellow", function(){
-        expect(main.lampBlockFiveMinute(5)).toBe('Y');
-        expect(main.lampBlockFiveMinute(10)).toBe('YY');
-        expect(main.lampBlockFiveMinute(15)).toBe('YYR');
+        expect(main.lampBlockFiveMinute(5)).toBe('1Y');
+        expect(main.lampBlockFiveMinute(10)).toBe('2Y');
+        expect(main.lampBlockFiveMinute(15)).toBe('2YR');
         expect(main.lampBlockFiveMinute(50)).toBe('YYRYYRYYRY');
     });
     
     it("should turn on red lights every single hour", function(){
-        expect(main.lampSingleHour(1)).toBe('R');
-        expect(main.lampSingleHour(3)).toBe('R');
-        expect(main.lampSingleHour(17)).toBe('RRR');
+        expect(main.lampSingleHour(1)).toBe('1R');
+        expect(main.lampSingleHour(3)).toBe('1R');
+        expect(main.lampSingleHour(17)).toBe('3RRR');
     });
 
 });
