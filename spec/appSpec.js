e6ee95c677648e494484c2d789d788165ecbc11b
Joshua <joshua.basele@student.vinci.be>
2024-11-14 12:16:29 +0100

    new : Fonction Single Hour

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -4,23 +4,28 @@ const main = new Main();
 
 describe("Berlin clock", () => {
 
-    it("should light up yellow lamps every single minute", function() {
+    it("should light up yellow lamps every minute in the last line", function() {
         expect(main.lampSingleMinute(1)).toBe('Y');
         expect(main.lampSingleMinute(3)).toBe('YYY');
         expect(main.lampSingleMinute(17)).toBe('YY');
     });
 
-    it("should turn on a red light every 3 lights and the others are yellow", function(){
+    it("should turn on a red light every 3 lights, the others being yellow in the 5-minute block line", function(){
         expect(main.lampBlockFiveMinute(5)).toBe('Y');
         expect(main.lampBlockFiveMinute(10)).toBe('YY');
         expect(main.lampBlockFiveMinute(15)).toBe('YYR');
         expect(main.lampBlockFiveMinute(50)).toBe('YYRYYRYYRY');
     });
     
-    it("should turn on red lights every single hour", function(){
+    it("should turn on the red lights every hour in the simple hour line", function(){
         expect(main.lampSingleHour(1)).toBe('R');
-        expect(main.lampSingleHour(3)).toBe('R');
-        expect(main.lampSingleHour(17)).toBe('RRR');
+        expect(main.lampSingleHour(3)).toBe('RRR');
+        expect(main.lampSingleHour(17)).toBe('RR');
     });
+
+    it("should light red lamps every 5 hours in the first line", function(){
+     
+    })
+
 
 });
--- src/app.js
+++ src/app.js
@@ -1,19 +1,22 @@
 export class Main{
 
         lampSingleMinute(minute) {
-            let lamp = minute % 5
-            return 'Y'.repeat(lamp);
+            return 'Y'.repeat(minute % 5);
         }  
         
         lampBlockFiveMinute(minute){
             let result = '';
-
             for(let i = 5; i <= minute; i+=5){
                 if(i % 15 == 0)
                     result += 'R';
                 else result += 'Y';
             }
-
             return result;
         }
+
+        lampSingleHour(hour){
+            return 'R'.repeat(hour % 5);
+        }
+
+        
 }
